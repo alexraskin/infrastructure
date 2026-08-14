17300ca757f8af24286520809b913294e0b4ff70
Alex Raskin <alexraskin@fastmail.fm>
2026-08-14 15:40:02 -0700

    remove key file

--- 01-cloud-edge/nixos/hosts/oracle-edge/secrets.nix
+++ 01-cloud-edge/nixos/hosts/oracle-edge/secrets.nix
@@ -4,7 +4,7 @@
     defaultSopsFile = ./secrets.sops.yaml;
 
     age = {
-      keyFile = "/var/lib/sops-nix/key.txt";
+      # keyFile = "/var/lib/sops-nix/key.txt";
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
     gnupg.sshKeyPaths = [ ];
